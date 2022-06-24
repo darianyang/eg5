d8869f8d536e0dadb59a05142c4b978b768af5ec
Darian T. Yang <dty7@pitt.edu>
2022-06-24 11:00:56 -0400

    sasa script is working now

--- eg5_adpmon/we/eg5_template/common_files/analyze_sasa.py
+++ eg5_adpmon/we/eg5_template/common_files/analyze_sasa.py
@@ -27,9 +27,14 @@ def calc_sasa(selection):
     traj = traj_parent+traj_segment
 
     #traj = mdtraj.load(f"{wpath}/eg5_2022.pdb")
+    #traj = mdtraj.load("../bstates/bstate.ncrst", top="eg5_holo.prmtop")
 
     # select a portion of the entire system
-    sel = traj.topology.select(selection)    
+    # note that some amber models (e.g. TIP4P) will have virtual sites (VS)
+    # these have no dedicated radii in the MDTraj _ATOMIC_RADII library
+    # https://github.com/mdtraj/mdtraj/blob/master/mdtraj/geometry/sasa.py#L39
+    # https://github.com/mdtraj/mdtraj/issues/1151
+    sel = traj.topology.select(selection + " and not element VS")
 
     # slice the trajectory to get atoms coordinates for the selection
     sel_atoms = traj.atom_slice(sel)
@@ -44,7 +49,8 @@ receptor = calc_sasa("resid 23 to 29 or resid 71 to 77 or resid 80 \
                      or resid 102 to 117 or resid 131 or resid 231 \
                      to 232 or resid 264 to 269 or resid 273 or \
                      resid 332 or resid 334 to 337")
-adp = calc_sasa("resid 369")
+adp = calc_sasa("resname ADP")
+#adp = calc_sasa("resid 369")
 
 np.savetxt("rec_sasa.dat", receptor) 
 np.savetxt("lig_sasa.dat", adp)
