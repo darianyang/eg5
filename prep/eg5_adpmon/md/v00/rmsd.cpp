32218d60e056ba1fb0d321358c1935525f7fa62a
Darian T. Yang <dty7@pitt.edu>
2022-07-07 10:37:06 -0400

    ran rmsd analysis of 200ns std sims for 1x88 and apo

--- prep/eg5_adpmon/md/v00/rmsd.cpp
+++ prep/eg5_adpmon/md/v00/rmsd.cpp
@@ -1,20 +1,17 @@
-parm 1x88_solv.prmtop
-trajin 03_eq1.nc 1 last 10
-trajin 04_eq2.nc 1 last 10
-trajin 05_eq3.nc 1 last 10
-trajin 25ns.nc 1 last 10
+parm 1x88_dry.prmtop
+trajin 06_prod.nc 1 last 100
 autoimage
 reference 1x88_solv.pdb [REF]
-rms prot :1-368&!@H= out equil/rms_prot_heavy.dat ref [REF]
-rms complex :1-368,370-371&!@H= out equil/rms_complex_heavy.dat ref [REF] nofit
-rms adp :370&!@H= out equil/rms_adp_heavy.dat ref [REF] nofit
-rms mon :371&!@H= out equil/rms_mon_heavy.dat ref [REF] nofit
+rms prot :1-368&!@H= out 200ns/rms_prot_heavy.dat ref [REF]
+rms complex :1-368,370-371&!@H= out 200ns/rms_complex_heavy.dat ref [REF] nofit
+rms adp :370&!@H= out 200ns/rms_adp_heavy.dat ref [REF] nofit
+rms mon :371&!@H= out 200ns/rms_mon_heavy.dat ref [REF] nofit
 # rmsf
 average crdset avg_crd
 run
 rms :1-368@CA,C,O,N ref avg_crd
-atomicfluct out equil/rmsf.dat :1-368@CA,C,O,N byres
-rms protcut :18-364&!@H= out equil/rms_protcut_heavy.dat ref [REF]
-rms complexcut :18-364,370-371&!@H= out equil/rms_complexcut_heavy.dat ref [REF] nofit
+atomicfluct out 200ns/rmsf.dat :1-368@CA,C,O,N byres
+rms protcut :18-364&!@H= out 200ns/rms_protcut_heavy.dat ref [REF]
+rms complexcut :18-364,370-371&!@H= out 200ns/rms_complexcut_heavy.dat ref [REF] nofit
 go
 quit
